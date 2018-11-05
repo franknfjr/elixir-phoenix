61fa304397f2eaa57c61635dc9e88410dae860a6
franknfjr <frankdonascimentojr@gmail.com>
2018-11-05 01:32:07 -0300

    session controller

--- dia4/chat/lib/chat_web/controllers/session_controller.ex
+++ dia4/chat/lib/chat_web/controllers/session_controller.ex
@@ -4,4 +4,19 @@ defmodule ChatWeb.SessionController do
   def new(conn, _params) do
     render(conn, "new.html")
   end
+
+  def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
+    case Auth.sign_in(email, password) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user_id, user.id)
+        |> put_flash(:info, "You have successfully signed in!")
+        |> redirect(to: room_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Invalid Email/Password")
+        |> render("new.html")
+    end
+  end
 end
