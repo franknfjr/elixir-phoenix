81051ba015a077eb53b952314524e33aed9a329c
franknfjr <frankdonascimentojr@gmail.com>
2018-11-05 01:46:42 -0300

    importando comeonin e bcrypt package

--- dia4/chat/mix.exs
+++ dia4/chat/mix.exs
@@ -41,7 +41,9 @@ defmodule Chat.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 1.0"}
     ]
   end
 
