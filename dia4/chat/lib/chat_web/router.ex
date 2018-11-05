479ec6daaee499e01fb73debea89283c89a410f4
franknfjr <frankdonascimentojr@gmail.com>
2018-11-05 01:06:58 -0300

    session mvc

--- dia4/chat/lib/chat_web/router.ex
+++ dia4/chat/lib/chat_web/router.ex
@@ -18,6 +18,8 @@ defmodule ChatWeb.Router do
     pipe_through(:browser)
 
     resources("/", RoomController)
+
+    resources("/sessions", SessionController, only: [:new, :create])
   end
 
   # Other scopes may use custom stacks.
